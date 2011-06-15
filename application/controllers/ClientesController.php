e3fe00fdbaa8802e46a32c369f4067aa829f494e
Carlos A. Gonzalez <betology@gmail.com>
2011-06-15 00:14:40 -0500

    agregue 3 acciones al controlador de Clientes. add, edit y delete

--- application/controllers/ClientesController.php
+++ application/controllers/ClientesController.php
@@ -13,6 +13,29 @@ class ClientesController extends Zend_Controller_Action
         // action body
     }
 
+    public function addAction()
+    {
+        // action body
+    }
+
+    public function editAction()
+    {
+        // action body
+    }
+
+    public function deleteAction()
+    {
+        // action body
+    }
+
 
 }
+
+
+
+
+
+
+
+
 
--- tests/application/controllers/ClientesControllerTest.php
+++ tests/application/controllers/ClientesControllerTest.php
@@ -26,8 +26,64 @@ class ClientesControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
             );
     }
 
+    public function testAddAction()
+    {
+        $params = array('action' => 'add', 'controller' => 'Clientes', 'module' => 'default');
+        $url = $this->url($this->urlizeOptions($params));
+        $this->dispatch($url);
+        
+        // assertions
+        $this->assertModule($params['module']);
+        $this->assertController($params['controller']);
+        $this->assertAction($params['action']);
+        $this->assertQueryContentContains(
+            'div#view-content p',
+            'View script for controller <b>' . $params['controller'] . '</b> and script/action name <b>' . $params['action'] . '</b>'
+            );
+    }
+
+    public function testEditAction()
+    {
+        $params = array('action' => 'edit', 'controller' => 'Clientes', 'module' => 'default');
+        $url = $this->url($this->urlizeOptions($params));
+        $this->dispatch($url);
+        
+        // assertions
+        $this->assertModule($params['module']);
+        $this->assertController($params['controller']);
+        $this->assertAction($params['action']);
+        $this->assertQueryContentContains(
+            'div#view-content p',
+            'View script for controller <b>' . $params['controller'] . '</b> and script/action name <b>' . $params['action'] . '</b>'
+            );
+    }
+
+    public function testDeleteAction()
+    {
+        $params = array('action' => 'delete', 'controller' => 'Clientes', 'module' => 'default');
+        $url = $this->url($this->urlizeOptions($params));
+        $this->dispatch($url);
+        
+        // assertions
+        $this->assertModule($params['module']);
+        $this->assertController($params['controller']);
+        $this->assertAction($params['action']);
+        $this->assertQueryContentContains(
+            'div#view-content p',
+            'View script for controller <b>' . $params['controller'] . '</b> and script/action name <b>' . $params['action'] . '</b>'
+            );
+    }
+
 
 }
+
+
+
+
+
+
+
+
 
 
 
