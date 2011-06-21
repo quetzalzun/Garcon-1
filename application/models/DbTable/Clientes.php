72be15fc7ebb9d8ca997fa5142a270cae9f33791
Carlos A. Goznalez <betology@gmail.com>
2011-06-20 19:37:55 -0500

    agregue las metodos para interfaceear la base de datos

--- application/models/DbTable/Clientes.php
+++ application/models/DbTable/Clientes.php
@@ -5,6 +5,42 @@ class Application_Model_DbTable_Clientes extends Zend_Db_Table_Abstract
 
     protected $_name = 'clientes';
 
+    public function getAlbum($id)
+    {
+        $id = (int)$id;
+        $row = $this->fetchRow('id = ' . $id);
+        if (!$row) {
+            throw new Exception("Could not find row $id");
+        }
+        return $row->toArray();
+    }
+
+    public function addAlbum($nombre, $correo, $telefono, $clave)
+    {
+        $data = array(
+            'nombre' => $nombre,
+            'correo' => $correo,
+            'telefono' => $telefono,
+            'clave' => $title,
+            );
+            $this->insert($data);
+    } 
+
+    public function updateAlbum($nombre, $correo, $telefono, $clave)
+    {
+        $data = array(
+            'nombre' => $nombre,
+            'correo' => $correo,
+            'telefono' => $telefono,
+            'clave' => $title,
+            );
+            $this->update($data, 'id = '. (int)$id);
+    } 
+
+    public function deleteAlbum($id)
+    {
+        this->delete('id =' . (int)$id);
+    }
 
 }
 
