dcb529029e7e1a75c8fb572620fe4574184a5994
Renich Bon Ciric <renich@woralelandia.com>
2011-07-19 20:17:06 -0500

    corrección de nombres de variables, estilo e indentación
    Se corrigieron los nombres de las variables de inglés a español.
    Se corrigió el estilo en general; particularmente, el espaciado entre paréntesis.
    Se corrigió la indentación de los métodos.

--- application/models/DbTable/Productos.php
+++ application/models/DbTable/Productos.php
@@ -4,43 +4,44 @@ class Application_Model_DbTable_Productos extends Zend_Db_Table_Abstract
 {  
     protected $_name = 'productos';
 
-    public function getProducto($id)
+    public function getProducto( $id )
     {
-        $id = (int)$id;
-        $row = $this->fetchRow('id = ' . $id);
+        $id = (int) $id;
+        $row = $this->fetchRow( 'id = ' . $id );
 
-        if (!$row) {
-            throw new Exception("No puede encontrar el registro $id");
+        if ( !$row ) {
+            throw new Exception( "No puede encontrar el registro $id" );
         }
         
         return $row->toArray();
-     }
+    }
 
-     public function addProducto($name, $description, $price, $quantity)
-     {
-         $data = array(
-         'name' => $name,
-         'description' => $description,
-         'price' => $price,
-         'quantity' => $quantity, );
+    public function addProducto( $nombre, $descripcion, $precio, $existencia )
+    {
+        $data = array(
+            'nombre' => $nombre,
+            'descripcion' => $descripcion,
+            'precio' => $precio,
+            'existencia' => $existencia, 
+        );
               	    
-          $this->insert($data);
-      }
+        $this->insert($data);
+    }
 
-      public function updateProducto($id, $name, $price, $quantity)
-      {
-          $data = array(
-          'name' => $name,
-          'description' => $description, 
-          'price' => $price,
-          'quantity' => $quantity,
-          );
+    public function updateProducto( $id, $nombre, $descripcion, $precio, $existencia )
+    {
+        $data = array(
+            'nombre' => $nombre,
+            'descripcion' => $descripcion, 
+            'precio' => $precio,
+            'existencia' => $existencia,
+        );
 
-          $this->update($data, 'id = '. (int)$id);
-      }
+        $this->update( $data, 'id = '. (int) $id );
+    }
 
-     public function deleteProducto($id)
-     {
-          $this->delete('id =' . (int)$id);
-     }
+    public function deleteProducto( $id )
+    {
+        $this->delete( 'id =' . (int) $id );
+    }
 }
