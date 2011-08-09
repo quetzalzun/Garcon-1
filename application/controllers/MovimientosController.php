3d00021a013e2558056c052ba27d601d3f7e18e8
Sergio Gutierrez <ark.sega@gmail.com>
2011-08-09 00:06:47 -0500

    agregué el controlador Movimientos y dos acciones
    Las acciones son adquirir y donar.

---
+++ application/controllers/MovimientosController.php
@@ -0,0 +1,32 @@
+<?php
+
+class MovimientosController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function adquirirAction()
+    {
+        // action body
+    }
+
+    public function donarAction()
+    {
+        // action body
+    }
+
+
+}
+
+
+
+
+
